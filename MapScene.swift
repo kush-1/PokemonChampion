fd6745a8194ed0cb6170de84d657bd200adfd9a4
kush-1 <kdd4203@gmail.com>
2025-04-29 15:23:15 -0400

    updated title screen

---
+++ MapScene.swift
@@ -0,0 +1,14 @@
+//
+//  MapScene.swift
+//  PokemonChampion
+//
+//  Created by Kush Desai on 4/28/25.
+//
+
+import SpriteKit
+
+class MapScene: SKScene {
+    override func didMove(to view: SKView) {
+        backgroundColor = .green  // just a placeholder to verify it loads
+    }
+}
--- PokemonChampion/MapScene.swift
+++ PokemonChampion/MapScene.swift
@@ -5,3 +5,10 @@
 //  Created by Kush Desai on 4/28/25.
 //
 
+import SpriteKit
+
+class MapScene: SKScene {
+    override func didMove(to view: SKView) {
+        backgroundColor = .green  // just a placeholder to verify it loads
+    }
+}
